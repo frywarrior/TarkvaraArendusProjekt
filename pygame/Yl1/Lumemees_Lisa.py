5f7676831abe4076f7ced4a2bc485696ce5c6f74
frywarrior <fkikkas@gmail.com>
2022-03-29 14:05:37 +0300

    lumemees

--- pygame/Yl1/Lumemees_Lisa.py
+++ pygame/Yl1/Lumemees_Lisa.py
@@ -3,11 +3,18 @@ import pygame
 import sys
 # impordib mooduli sys
 import time as t
+
 i = 0
+u = 20
 screen = pygame.display.set_mode([300, 300])
 #
 pygame.display.set_caption("Lumemees")
 # Paneb display nime lumemeheks
+
+def pilv(x, y):
+    pygame.draw.circle(screen, [255, 255, 255], [x, y], 15, 0)
+    pygame.draw.circle(screen, [255, 255, 255], [x + 15, y], 16, 0)
+    pygame.draw.circle(screen, [255, 255, 255], [x + 30, y], 15, 0)
 while True:
     screen.fill([0, 191, 255])
     # Täidab ekraani sinisega
@@ -25,7 +32,6 @@ while True:
     pygame.draw.circle(screen, [255, 255, 255], [150, 235], 50, 0)
     # pygame.draw.circle(screen, värv, tsentri_pos, raadius, joone_paksus)
 
-
     #
     ### LISA
     #
@@ -34,14 +40,23 @@ while True:
     pygame.draw.circle(screen, [230, 230, 230], [100, 450], 195, 0)
     pygame.draw.circle(screen, [230, 230, 230], [200, 350], 85, 0)
     pygame.draw.circle(screen, [230, 230, 230], [0, 450], 190, 0)
-    #Lumi
+    # Lumi
 
     pygame.draw.circle(screen, [255, 255, 26], [300, 0], 50, 0)
     pygame.draw.circle(screen, [255, 255, 77], [300, 0], 40, 0)
+
+
     # Päike
 
 
-
+    if u < 330:
+        pilv(u,50)
+        pilv(u + 50, 15)
+        pilv(u - 50, 75)
+        u += 10
+    else:
+        u = -70
+    # Pilv
 
     if i != 2:
         pygame.draw.line(screen, [102, 51, 0], [50, 190], [200, 190], 2)
@@ -79,9 +94,8 @@ while True:
         pygame.draw.line(screen, [153, 102, 51], [125, 190], [130, 200], 2)
         # vasak käsi
 
-
-
-    pygame.draw.polygon(screen, [50, 50, 50], [[125, 70], [175, 70], [175, 60], [165, 60], [165, 30], [135, 30], [135, 60], [125, 60]], 0)
+    pygame.draw.polygon(screen, [50, 50, 50],
+                        [[125, 70], [175, 70], [175, 60], [165, 60], [165, 30], [135, 30], [135, 60], [125, 60]], 0)
     pygame.draw.polygon(screen, [255, 255, 255], [[135, 60], [165, 60], [165, 55], [135, 55]], 0)
     pygame.draw.circle(screen, [200, 0, 0], [165, 55], 5, 0)
     pygame.draw.circle(screen, [255, 0, 0], [165, 55], 3, 0)
@@ -94,7 +108,6 @@ while True:
 
     pygame.display.update()
     # Uuendab akent viimaste muudatustega
-
 
     for event in pygame.event.get():
         # Otsib kas on mingi "Event" toimunud
