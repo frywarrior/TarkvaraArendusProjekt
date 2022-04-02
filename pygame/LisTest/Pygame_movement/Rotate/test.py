7091ef14aeb084e3362c8c6867c0b1ecdf937053
frywarrior <fkikkas@gmail.com>
2022-04-02 14:39:38 +0300

    test

--- pygame/LisTest/Pygame_movement/Rotate/test.py
+++ pygame/LisTest/Pygame_movement/Rotate/test.py
@@ -1,8 +1,9 @@
 import pygame
-
+import math
 
 x = 150
 y = 0
+"""
 def rotate_triangle(center, scale, mouse_pos):
     vMouse = pygame.math.Vector2(mouse_pos)
     vCenter = pygame.math.Vector2(center)
@@ -14,21 +15,39 @@ def rotate_triangle(center, scale, mouse_pos):
 
     triangle_points = [(vCenter + p * scale) for p in rotated_point]
     return triangle_points
+"""
+
+loc = (150, 150)
+
+
+def rotate_polygon(center, scale, mouse_pos):
+    dx = mouse_pos[0] - center[0]
+    # print(f"dx.{dx}")
+    dy = mouse_pos[1] - center[1]
+    # print(f"dy.{dy}")
+    le = math.sqrt(dx * dx + dy * dy)
+    dx, dy = (dx * scale / le, dy * scale / le) if le > 0 else (1, 0)
+
+    pts = [(1, 0.5), (2, 0.5), (2, -0.5), (1, -0.5)]
+    pts = [(center[0] + p[0] * dx + p[1] * dy, center[1] + p[0] * dy - p[1] * dx) for p in pts]
+    return pts
 
 
 disp = pygame.display.set_mode((300, 300))
 
-
 run = True
 while run:
-
+    keys = pygame.key.get_pressed()
+    mouse_position = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if keys[pygame.K_m]:
+            loc = mouse_position
 
-    mouse_position = pygame.mouse.get_pos()
-    points = rotate_triangle((150, 150), 10, mouse_position)
+    points = rotate_polygon(loc, 10, mouse_position)
 
     pygame.Surface.fill(disp, (255, 255, 255))
-    pygame.draw.polygon(disp, (0, 0, 0), points)
+    pygame.draw.circle(disp, (0, 0, 0), loc, 15)
+    pygame.draw.polygon(disp, (150, 150, 150), points)
     pygame.display.update()
