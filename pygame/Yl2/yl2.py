b2038fcb99877d05b7e620323eabb42eef816176
johannes.kaup <johannes.kaup@vikk.ee>
2022-04-08 09:55:47 +0300

    safsf

--- pygame/Yl2/yl2.py
+++ pygame/Yl2/yl2.py
@@ -3,10 +3,17 @@ pygame.init()
 #ekraani seaded
 screen=pygame.display.set_mode([640,480])
 pygame.display.set_caption("yl2_pilt")
-screen.fill([255, 255, 255])
 
-font = pygame.font.Font(None, 30)
-text = font.render("Hello PyGame", True, [0,0,0])
-screen.blit(text, [200,200])
+main = True
 
-pygame.display.flip()
+while main == True:
+    screen.fill([255, 255, 255])
+
+    font = pygame.font.Font(None, 30)
+    text = font.render("Tere, olen sinu nimi", True, [0, 0, 0])
+    screen.blit(text, [200, 200])
+
+    bg = pygame.image.load("yl2_pilt.jpg")
+    screen.blit(bg, [0, 0])
+
+    pygame.display.flip()
